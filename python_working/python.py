6e70d98566b8a77b9d40ffc549bf6ebda5966c83
Usamakhan56486 <khangpc444@gmail.com>
2026-05-09 12:51:31 +0500

    remove jarvis

--- python_working/python.py
+++ python_working/python.py
@@ -1,4 +1,6 @@
 import pyttsx3 
+import speech_recognition as sr
+
 
 engine = pyttsx3.init()
 
@@ -11,4 +13,20 @@ def speak (command):
     engine.say(command)
     engine.runAndWait()
 
-speak('how are you ')
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+try:
+    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+speak('''hi
+
+''')
+
+
